2d50c2dff629ea918ab8115078177acf2d4fe1ee
Sehnsucht <Credgate1992@hotmail.com>
2020-12-10 23:25:16 -0500

    Main Push
    This is the initial push to github

---
+++ Assets/Scripts/InputSystem/GameplayContext.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameplayContext : InputContext
+{
+    public override void HorizontalButtonPress(float value)
+    {
+        Debug.Log(Constants.gameplayContext + " " + Constants.input + " " + Constants.horizontal + ": " + Input.GetAxis(Constants.horizontal));
+    }
+
+    public override void VerticalButtonPress(float value)
+    {
+        Debug.Log(Constants.gameplayContext + " " + Constants.input + " " + Constants.vertical + ": " + Input.GetAxis(Constants.vertical));
+    }
+
+    public override void Action1ButtonPress()
+    {
+        Debug.Log(Constants.gameplayContext + " " + Constants.input + " " + Constants.action1 + ": " + Input.GetButtonDown(Constants.action1));
+    }
+
+    public override void Action2ButtonPress()
+    {
+        Debug.Log(Constants.gameplayContext + " " + Constants.input + " " + Constants.action2 + ": " + Input.GetButtonDown(Constants.action2));
+    }
+
+    public override void Menu1ButtonPress()
+    {
+        Debug.Log(Constants.gameplayContext + " " + Constants.input + " " + Constants.menu1 + ": " + Input.GetButtonDown(Constants.menu1));
+    }
+
+    public override void Menu2ButtonPress()
+    {
+        Debug.Log(Constants.gameplayContext + " " + Constants.input + " " + Constants.menu2 + ": " + Input.GetButtonDown(Constants.menu2));
+    }
+}
---
+++ Assets/Scripts/InputSystem/InputContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InputContext
+{
+    public abstract void HorizontalButtonPress(float value);
+
+    public abstract void VerticalButtonPress(float value);
+
+    public abstract void Action1ButtonPress();
+
+    public abstract void Action2ButtonPress();
+
+    public abstract void Menu1ButtonPress();
+
+    public abstract void Menu2ButtonPress();
+}

e8c50afb47e1205c3b4f3b3d124f87a8d591d397
leivp <leivp@amazon.com>
2020-12-22 20:35:08 -0800

    Integrated movement with animations

--- RZU-7 Project/Assets/Scripts/AnimationSystem/ActorAnimations.cs
+++ RZU-7 Project/Assets/Scripts/AnimationSystem/ActorAnimations.cs
@@ -1,19 +1,59 @@
 ﻿using UnityEngine;
-using UnityEditor.Animations;
 
 
 public class ActorAnimations : MonoBehaviour
 {
     [SerializeField]
-    AnimatorController animator; // Should be set in Editor
+    Animator animator; // Should be set in Editor
+    float lastX = 0f;
+    float lastY = 0f;
+    float zeroApproximationBoundary = 0.01f;
+
 
     void Start()
     {
-        animator = gameObject.GetComponent<AnimatorController>();
+        if (animator == null)
+        {
+            animator = gameObject.GetComponent<Animator>();
+        }
     }
 
     public void SetMovementValues(float x, float y, float speed)
     {
-        // Set animator movement variables here
+        animator.SetFloat(AnimationConstants.lastX, lastX);
+        animator.SetFloat(AnimationConstants.lastY, lastY);
+
+        animator.SetFloat(AnimationConstants.movingX, x);
+        animator.SetFloat(AnimationConstants.movingY, y);
+
+        CheckLastMovementInput(x, y);
+
+        SetIsMoving(x, y);
+    }
+
+    void CheckLastMovementInput(float x, float y)
+    {
+        if ((Mathf.Abs(x) + Mathf.Abs(y)) > 0)
+        {
+            CheckAxisLastMovementInput(x, ref lastX);
+            CheckAxisLastMovementInput(y, ref lastY);
+        }
+    }
+
+    void CheckAxisLastMovementInput(float newValue, ref float lastValue)
+    {
+        if (Mathf.Abs(newValue) > zeroApproximationBoundary)
+        {
+            lastValue = newValue > 0f ? 1f : -1f;
+        }
+        else
+        {
+            lastValue = 0f;
+        }
+    }
+
+    void SetIsMoving(float x, float y)
+    {
+        animator.SetBool(AnimationConstants.isMoving, !Mathf.Approximately(x, 0f) || !Mathf.Approximately(y, 0f));
     }
 }
---
+++ RZU-7 Project/Assets/Scripts/AnimationSystem/AnimationConstants.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationConstants
+{
+    public static readonly string lastX = "LastX";
+    public static readonly string lastY = "LastY";
+    public static readonly string movingX = "MovingX";
+    public static readonly string movingY = "MovingY";
+    public static readonly string isMoving = "IsMoving";
+}

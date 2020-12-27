acaf27f8e34f37924e826fe3eaadf208fa711b35
leivp <leivp@amazon.com>
2020-12-27 11:17:37 -0800

    Implemented interaction system

---
+++ RZU-7 Project/Assets/Scripts/ItemInteractions/InteractableItem.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Governs behavior for items the player can interact with
+/// </summary>
+/// <param name="spriteRenderer">The sprite renderer for the item</param>
+/// <param name="itemCollider">The item's 2D interaction trigger collider</param>
+/// <param name="sprites">The sprites that denote the interaction taking place</param>
+/// <param name="timeDelta">The amount of time to wait before rendering the next sprite</param>
+/// <param name="triggerSequence">Denotes whether or not to trigger the sequence</param>
+/// <param name="isOn">Denotes whether or not the item is in the "On" state</param>
+public class InteractableItem : MonoBehaviour
+{
+    [SerializeField]
+    SpriteRenderer spriteRenderer;
+    [SerializeField]
+    Collider2D itemCollider;
+    [SerializeField]
+    Interaction interaction;
+    [SerializeField]
+    List<Sprite> sprites;
+    [SerializeField]
+    float timeDelta = InteractionConstants.defaultTimeDelta;
+    bool triggerSequence;
+    bool isOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        if (itemCollider == null)
+        {
+            itemCollider = GetComponent<Collider2D>();
+        }
+    }
+
+    void Update()
+    {
+        if (triggerSequence)
+        {
+            StartCoroutine(RunSpriteSequence());
+        }
+    }
+
+    /// <summary>
+    /// sets the SpriteRenderer sprite to each sprite in the list on a timer interval.
+    /// </summary>
+    /// <returns>
+    /// IEnumerator object. This is a coroutine so...magic.
+    /// </returns>
+    IEnumerator RunSpriteSequence()
+    {
+        foreach (Sprite sprite in sprites)
+        {
+            spriteRenderer.sprite = sprite;
+            triggerSequence = false;
+
+            yield return new WaitForSeconds(timeDelta);
+        }
+
+        sprites.Reverse();
+
+        yield return null;
+    }
+
+    /// <summary>
+    /// Triggers interaction sequence when the player interacts with the item
+    /// </summary>
+    /// <param name="collision">The player's interaction collider</param>
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        triggerSequence = collision.gameObject.CompareTag(InteractionConstants.interactTag);
+        isOn = !isOn;
+        interaction.OnInteract(isOn);
+    }
+}
---
+++ RZU-7 Project/Assets/Scripts/ItemInteractions/InteractionConstants.cs
@@ -0,0 +1,5 @@
+﻿public static class InteractionConstants
+{
+    public static readonly float defaultTimeDelta = 0.3f;
+    public static readonly string interactTag = "Interact";
+}

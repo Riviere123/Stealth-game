241e450a334af158f6a8e7ca31e257b25d5d94a2
Sehnsucht <Credgate1992@hotmail.com>
2020-12-30 01:41:05 -0500

    revert

---
+++ RZU-7 Project/Assets/Scripts/StatusEffects/StatusEffects.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Status Effect", menuName = "Status's")]
+[SerializeField]
+public class StatusEffects : ScriptableObject
+{
+    public string Name;
+}

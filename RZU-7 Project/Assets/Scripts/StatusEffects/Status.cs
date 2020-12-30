beba87bc29294338dd68bc2e69afbb7f6fa75ece
Sehnsucht <Credgate1992@hotmail.com>
2020-12-30 01:41:49 -0500

    revert

--- RZU-7 Project/Assets/Scripts/StatusEffects/Status.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Status : MonoBehaviour
-{
-    public List<StatusEffects> statusEffects = new List<StatusEffects>();
-}

9d6953ea83aed783acaa6372c6ebd56f7e7f5c8e
rzu-7 <50757340+rzu-7@users.noreply.github.com>
2020-12-30 21:09:55 -0800

    Item Interaction System (#38)
    * Implemented interaction system
    
    * Updated test scene
    
    * Fixed bug caused by spamming interaction
    
    * Fixed outdated docstring
    
    Co-authored-by: leivp <leivp@amazon.com>

---
+++ RZU-7 Project/Assets/Scripts/ItemInteractions/Interaction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Abstracts the logic to implement for when the player interacts with an object
+/// </summary>
+public abstract class Interaction : MonoBehaviour
+{
+    /// <summary>
+    /// Do logic when the player interacts with item
+    /// </summary>
+    /// <param name="isOn">Whether or not the item is in the "On" state</param>
+    public abstract void OnInteract(bool isOn);
+}
---
+++ RZU-7 Project/Assets/Scripts/ItemInteractions/Tests/TestCabinetInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCabinetInteraction : Interaction
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnInteract(bool isOn)
+    {
+        if (isOn)
+        {
+            Debug.Log("The player has opened the cabinet");
+        }
+        else
+        {
+            Debug.Log("The player has closed the cabinet");
+        }
+        
+    }
+}

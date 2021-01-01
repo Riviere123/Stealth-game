527ff7c3aed02f1cbf397b4078907dca4892a081
Sehnsucht <Credgate1992@hotmail.com>
2021-01-01 04:03:45 -0500

    Added time and gold counter

---
+++ RZU-7 Project/Assets/Scripts/Items/Valuable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Valuable : MonoBehaviour
+{
+    [SerializeField]
+    LevelLogic levelLogic;
+    [SerializeField]
+    int value = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            levelLogic.AddGold(value);
+            levelLogic.AddItem(GetComponent<SpriteRenderer>().sprite);
+            Destroy(gameObject);
+        }
+    }
+}
--- RZU-7 Project/Assets/Scripts/LevelLogic/LevelLogic.cs
+++ RZU-7 Project/Assets/Scripts/LevelLogic/LevelLogic.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 [System.Serializable]
 public struct KeyToDoor
@@ -17,8 +19,37 @@ public class LevelLogic : MonoBehaviour
     GameObject inventoryPanel;
     [SerializeField]
     GameObject itemImage;
+    [SerializeField]
+    TextMeshProUGUI goldText;
+    [SerializeField]
+    GameObject timeImage;
+    [SerializeField]
+    TextMeshProUGUI timeText;
+    [SerializeField]
+    string nextLevel;
+    [SerializeField]
+    int gold;
+    [SerializeField]
+    int levelTime;
+    [SerializeField]
+    int currentTime;
     public KeyToDoor[] sDoors;
 
+    private void Awake()
+    {
+        currentTime = levelTime;
+    }
+    private void Start()
+    {
+        StartCoroutine(CountDown());
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            LoadLevel(nextLevel);
+        }
+    }
     private void OnGUI()
     {
         for(int i = 0; i < sDoors.Length; i++)
@@ -37,6 +68,51 @@ public class LevelLogic : MonoBehaviour
     {
         GameObject tempObject = Instantiate(itemImage,inventoryPanel.transform);
         tempObject.GetComponent<Image>().sprite = sprite;
+    }
+    
+    public void AddGold(int newGold)
+    {
+        gold += newGold;
+        goldText.text = gold.ToString();
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Game Over!");
+    }
+    void LoadLevel(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    IEnumerator CountDown()
+    {
+        if (currentTime <= 0)
+        {
+            GameOver();
+        }
+
+        int minutes;
+        int seconds;
+
+        minutes = currentTime / 60;
+        seconds = currentTime % 60;
+        if(seconds < 10)
+        {
+            timeText.text = $"{minutes}:0{seconds}";
+        }
+        else
+        {
+            timeText.text = $"{minutes}:{seconds}";
+        }
+
+
+        if (currentTime != 0)
+        {
+            yield return new WaitForSeconds(1);
+            currentTime -= 1;
+            StartCoroutine(CountDown());
+        }
     }
 }
 

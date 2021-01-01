b4c97261bda53c4e231cd325b1a5373f2d8f12bb
Sehnsucht <Credgate1992@hotmail.com>
2020-12-31 20:42:14 -0500

    Redo Of Hud

---
+++ RZU-7 Project/Assets/Scripts/LevelLogic/Key.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField]
+    LevelLogic levelLogic;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            for(int i = 0; i < levelLogic.sDoors.Length; i++)
+            {
+                if(levelLogic.sDoors[i].Key == gameObject)
+                {
+                    levelLogic.sDoors[i].HasKey = true;
+                    levelLogic.AddItem(GetComponent<SpriteRenderer>().sprite);
+                    break;
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ RZU-7 Project/Assets/Scripts/LevelLogic/LevelLogic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct KeyToDoor
+{
+    public GameObject Key;
+    public GameObject Door;
+    public bool HasKey;
+}
+
+public class LevelLogic : MonoBehaviour
+{
+    [SerializeField]
+    GameObject inventoryPanel;
+    [SerializeField]
+    GameObject itemImage;
+    public KeyToDoor[] sDoors;
+
+    private void OnGUI()
+    {
+        for(int i = 0; i < sDoors.Length; i++)
+        {
+            if (sDoors[i].HasKey)
+            {
+                sDoors[i].Door.GetComponent<SpriteRenderer>().color = Color.green;
+            }
+            else
+            {
+                sDoors[i].Door.GetComponent<SpriteRenderer>().color = Color.red;
+            }
+        }
+    }
+    public void AddItem(Sprite sprite)
+    {
+        GameObject tempObject = Instantiate(itemImage,inventoryPanel.transform);
+        tempObject.GetComponent<Image>().sprite = sprite;
+    }
+}
+

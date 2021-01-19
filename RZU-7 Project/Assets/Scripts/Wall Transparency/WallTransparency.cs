f58f42b8adf074dce906c58f7b2b1dc233f1531e
Sehnsucht <Credgate1992@hotmail.com>
2021-01-18 20:51:13 -0500

    Fixed wall transparency script issue

---
+++ RZU-7 Project/Assets/Scripts/Wall Transparency/WallTransparency.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WallTransparency : MonoBehaviour
+{
+    Color transparent = new Color(1, 1, 1, .5f);
+    Color solid = new Color(1, 1, 1, 1);
+    Tilemap tilemap;
+    private void Awake()
+    {
+       tilemap = GetComponent<Tilemap>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            tilemap.color = transparent;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            tilemap.color = solid;
+        }
+    }
+}
